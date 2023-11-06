e519160616611c5662be8f7b3a4a726f61784dfc
root <gonzalopeder2@gmail.com>
2023-11-06 22:48:15 +0000

    Testeo de calendario. Por ahora va bien

--- src/routes/negocio/[slug]/+layout.svelte
+++ src/routes/negocio/[slug]/+layout.svelte
@@ -53,7 +53,12 @@
     </a>
   </div>
 </div>
-<div class="py-10">
+<div class="py-10 bg-white">
   <slot />
 </div>
+<footer class="fixed bottom-0 left-0 w-full text-white py-4 gradient-footer">
+  <div class="container mx-auto">
+    <p class="text-center">© 2023 Tu Sitio Web</p>
+  </div>
+</footer>
 
--- src/routes/negocio/[slug]/agenda/+page.svelte
+++ src/routes/negocio/[slug]/agenda/+page.svelte
@@ -1,26 +1,66 @@
 <script>
-  import calendario from '../../../../asset/calendario.png'
+  import Calendar from '@event-calendar/core';
+  import TimeGrid from '@event-calendar/time-grid';
+  import '@event-calendar/core/index.css';
+
+  let hugo = {
+    collaborator: 'Rosa',
+    client: 'Hugo'
+  }
+  let plugins = [TimeGrid];
+
+  let options = {
+    view: 'timeGridWeek',
+    locale: 'es',
+    events: [
+      // poner eventos: title, start, end.
+      {
+        title: 'Evento 1',
+        start: '2023-11-06T10:00:00',
+        end: '2023-11-06T18:00:00'
+      },
+      {
+        title: 'Evento 2',
+        start: '2023-11-06T14:00:00',
+        end: '2023-11-06T17:00:00'
+      },
+      {
+        title: 'Evento 3',
+        start: '2023-11-08T08:00:00',
+        end: '2023-11-08T15:00:00'
+      },
+      {
+        title: 'Evento 4',
+        start: '2023-11-09T08:00:00',
+        end: '2023-11-09T14:00:00'
+      },
+      {
+        title: 'Descanso de Rosa',
+        start: '2023-11-05',
+        end: '2023-11-05',
+        backgroundColor: '#9f53ff'
+      }
+      
+    ],
+    eventBackgroundColor: '#7A8ADA',
+    eventClassNames: ['iris_green', 'iris_blue', 'iris_purple', 'border', 'border-black', 'rounded-2xl'],
+    nowIndicator: true,
+    slotDuration: '00:30:00',
+    allDayContent: 'Dia libre',
+    noEventsContent: 'Aun no se ha agendado nadie',
+    headerToolbar: {start: 'title', center: '', end: 'prev,next'}
+  }
 </script>
 
-<div class='flex flex-grow h-full py-10'>
-  <div class=' flex flex-col w-1/4 items-center justify-center'>
-    <h1 class='text-4xl font-semibold mb-5'>Mis agendas</h1>
-    <div class='flex flex-col gap-1 text-xl'>
-      <div class=' flex flex-row gap-1'>
-        <input type="checkbox" id="colab1" value="colab1" />
-        <label for="colab1">Colaborador 1</label>
-      </div>
-      <div class=' flex flex-row gap-1'>
-        <input type="checkbox" id="colab2" value="colab2" />
-        <label for="colab2">Colaborador 2</label>
-      </div>
-      <div class=' flex flex-row gap-1'>
-        <input type="checkbox" id="colab3" value="colab3" />
-        <label for="colab3">Colaborador 3</label>
+<div class='flex flex-col h-full py-10 bg-white'>
+  <div class=' flex flex-col w-1/4 justify-start items-center'>
+      <h1 class='text-4xl font-semibold mb-5'>Mis agendas</h1>
+  </div>
+  <div class="flex justify-center items-center w-full px-10">
+    <div class='justify-center items-center w-full'>
+      <div class='bg-white ec-auto-dark text-black'>
+      <Calendar {plugins} {options} />
       </div>
     </div>
-  </div>
-  <div class='w-3/4'>
-    <img src={calendario} alt="calendario" class='w-3/4' />
   </div>
 </div>
